bda186957aec6b98ebbaeeffaee6c488cd188f06
vipul <vipul.jarvis@gmail.com>
2024-03-18 17:40:53 +0530

    - README.md file update

--- lib/src/constant/common_constant.dart
+++ lib/src/constant/common_constant.dart
@@ -4,5 +4,5 @@ const double ALLOW_IMAGE_FILE_SIZE_IN_MB = 3;
 
 ///Dates
 const String API_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
-final String CURRENT_TIME_STAMP =
+final String currentTimeStamp =
     DateTime.parse(DateTime.now().toString()).toString().replaceAll(":", "_");
